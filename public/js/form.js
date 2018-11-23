5ae10b3a9a1eb1bbed05267871b5602d3aae299c
cberry216 <christopherberry216@gmail.com>
2018-11-23 16:33:32 -0500

    Removed geolocation funcitonality due to errors.

--- public/js/form.js
+++ public/js/form.js
@@ -1,18 +1,11 @@
 /* global google */
 
-var map, autoMap, infoWindow;
+var autoMap;
 
-var currPosition = { coords: { latitude: 41.864995, longitude: -71.274469 } };
+// var currPosition;
 
 function initMap() {
-	options = {
-		center: { lat: 41.866254, lng: -71.249101 },
-		zoom: 17,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	};
 	autoMap = new google.maps.Map(document.getElementById('auto-map'));
-	map = new google.maps.Map(document.getElementById('map'), options);
-	infoWindow = new google.maps.InfoWindow();
 }
 
 function initAutocomplete() {
@@ -31,51 +24,9 @@ function initAutocomplete() {
 	var autocomplete = new google.maps.places.Autocomplete(input, options);
 }
 
-function getLocation() {
-	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(
-			position => {
-				currPosition = position;
-			},
-			() => {
-				console.log('location not found.');
-			}
-		);
-	} else {
-		console.log('geolocation not enabled.');
-	}
-}
-
 function run() {
 	initMap();
 	initAutocomplete();
-	getLocation();
 }
-
-function addMap() {
-	pos = {
-		lat: currPosition.coords.latitude,
-		lng: currPosition.coords.longitude
-	};
-	console.log(pos);
-	infoWindow.setPosition(pos);
-	infoWindow.setContent('6 Fuller Street, Rehoboth, MA, 02769');
-	infoWindow.open(map);
-}
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-	infoWindow.setPosition(pos);
-	infoWindow.setContent(
-		browserHasGeolocation
-			? 'Error: The Geolocation service failed.'
-			: "Error: Your browser doesn't support geolocation."
-	);
-	infoWindow.open(map);
-}
-
-// TODO: implement geocodeLatLng for reverse geocoding
-// function geocodeLatLng(geocoder, map, infoWindow) {}
 
 window.onload = run;
-
-document.getElementById('find-address').onclick = addMap;
